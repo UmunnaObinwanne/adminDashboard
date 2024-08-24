eba2167f739a9261edaa9d9439fb9e174ab96b2a
Mike <broadwaymarketingconsults@gmail.com>
2024-08-25 00:56:28 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import AdminJS from 'adminjs';
+import AdminJSExpress from '@adminjs/express';
+import * as AdminJSMongoose from '@adminjs/mongoose';
+import PageResource from './resources/pageResources.js'; // Correct path
+import { componentLoader } from './components/components.js';
+
+const app = express();
+
+dotenv.config();
+
+// Enable trust proxy
+app.set('trust proxy', 1);
+
+// Session middleware configuration
+app.use(session({
+  secret: process.env.MY_APP_COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: 'none'
+  }
+}));
+
+// Database connection
+const dbPassword = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://broadwaymarketingconsults:${dbPassword}@carmartuk.0chjo.mongodb.net/carmart?retryWrites=true&w=majority&appName=CarmartUK`;
+
+mongoose.connect(uri, {})
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+AdminJS.registerAdapter(AdminJSMongoose);
+const adminJs = new AdminJS({
+    resources: [PageResource],
+    componentLoader,
+  branding: {
+    logo: '/shopmart logo.jpg',
+  },
+  rootPath: '/admin',
+});
+const adminJsRouter = AdminJSExpress.buildRouter(adminJs);
+app.use(adminJs.options.rootPath, adminJsRouter);
+console.log(`AdminJS connected successfully at ${adminJs.options.rootPath}`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
